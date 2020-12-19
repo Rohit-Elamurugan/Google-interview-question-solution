bc7ec9b0d9ef611eedd593a927ce8ac026e0ba95
Rohit Elamurugan <48488024+Rohit-Elamurugan@users.noreply.github.com>
2020-12-19 22:42:03 +0530

    uploaded the code

---
+++ google-former-question.py
@@ -0,0 +1,48 @@
+def longestWord(array:list[str]):
+    longestWord = ''
+    for element in array:
+        if len(element) > len(longestWord):
+            longestWord = element
+    return longestWord
+
+def mapString(string:str):
+    map = {}
+    for i in range(len(string)):
+        letter = string[i]
+        if letter in map:
+            map[letter].append(i)
+        else:
+            map[letter] = [i]
+    return map
+
+def findNextIndex(array:list[int],minIndex:int):
+    for element in array:
+        if element >= minIndex:
+            return element+1
+    return False
+
+def isSubsequence(word:str,map:dict):
+    minIndex = 0
+    for letter in word:
+        if letter in map:
+            minIndex = findNextIndex(map[letter],minIndex)
+            if minIndex == False:
+                return False
+        else:
+            return False
+    return True
+
+def longestMatch(string:str,dictionary:list[str]):
+    subsequences = []
+    map = mapString(string)
+    for element in dictionary:
+        if isSubsequence(element,map):
+            subsequences.append(element)
+    return longestWord(subsequences)
+
+stringSequence = 'javascript'
+dictionary = ['art','vascular','avast','javas','vat']
+
+print(stringSequence)
+print(dictionary)
+print(longestMatch(stringSequence,dictionary))
